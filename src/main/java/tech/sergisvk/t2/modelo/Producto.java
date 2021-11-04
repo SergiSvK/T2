32acd9ce9a1df372550096b9b05e1f0a08005163
Sergi <s.k.valero@gmail.com>
2021-11-04 18:00:40 +0100

    fin de la documentación de las clases producto

--- src/main/java/tech/sergisvk/t2/modelo/Producto.java
+++ src/main/java/tech/sergisvk/t2/modelo/Producto.java
@@ -2,17 +2,12 @@ package tech.sergisvk.t2.modelo;
 
 import java.util.HashSet;
 import java.util.Set;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.URL;
 
 
@@ -23,36 +18,62 @@ public class Producto {
     @GeneratedValue
     private Long id;
 
+    /**
+     * Hace uso de la implementación isValid() de la clase @NotNull, además identidica que el tamaño/longitud del objeto
+     * suministrado es mayor a 1 y menor a 31.
+     */
     @NotEmpty
+    @Size(min = 1, max = 32)
     private String nombre;
 
     /**
      * Es un tipo de datos para almacenar datos de objetos grandes. Siendo BLOB y CLOB.
      * BLOB es para almacenar datos binarios y CLOB para almacenar datos de texto.
-     * Siendo la descripción del producto, nos interesa guardar datos de texto. ASí que usaremos CLOB
+     * Siendo la descripción del producto, nos interesa guardar datos de texto. ASí que usaremos CLO.
      */
     @Lob
     //@Column(name = "descripcion", columnDefinition="VARCHAR(128)") ¿?
     private String descripcion;
 
     @Min(0)
-    private float pvp;
+    private float pvp; //Precio de venta al público, siendo un minimo del 0
 
     private float descuento;
 
     @URL
     private String imagen;
 
+    /**
+     * No puede ser nulo, la anotación se define en la especificación Verification Bean. Esto significa que no se
+     * limita solo a las entidad. Por lo contrario también podemos usar el @NotNull en cualquier otro bean.
+     */
     @NotNull
+    /* 1:n Uno a muchos
+     * La forma de funcionar es a nivel de base de datos es que tenemos una id clave principal en tabla de Productos
+     * y tambien una id como clave externa en la tabla de puntacion.
+     */
     @ManyToOne
     private Categoria categoria;
 
+    /**
+     * 1:n
+     */
     @OneToMany(mappedBy="producto", cascade=CascadeType.ALL, orphanRemoval=true, fetch=FetchType.EAGER)
     private Set<Puntuacion> puntuaciones = new HashSet<Puntuacion>();
 
     public Producto() {
+        //void
     }
 
+    /**
+     * Información que necesitamos saber sobre el producto
+     * @param nombre que vaya a recibir
+     * @param descripcion detalles y caracteríticas
+     * @param pvp que tipo de impuesto se le aplica
+     * @param descuento si esta de oferta o no con la cantidad a descuento
+     * @param imagen URL de la imagen
+     * @param categoria donde seguarda
+     */
     public Producto(String nombre, String descripcion, float pvp, float descuento, String imagen, Categoria categoria) {
         this.nombre = nombre;
         this.descripcion = descripcion;
@@ -62,6 +83,7 @@ public class Producto {
         this.categoria = categoria;
     }
 
+    //Getters and Setters
     public Long getId() {
         return id;
     }

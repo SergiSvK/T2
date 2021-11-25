24159bd14bfdb43af26461277f4294106368f315
Sergi <s.k.valero@gmail.com>
2021-11-25 19:20:58 +0100

    catalogo

--- src/main/java/tech/sergisvk/ecotech/controlador/Backend.java
+++ src/main/java/tech/sergisvk/ecotech/controlador/Backend.java
@@ -3,6 +3,7 @@ package tech.sergisvk.ecotech.controlador;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import tech.sergisvk.ecotech.servicios.ProductoService;
 import tech.sergisvk.ecotech.utilidades.Util;
 
 @Controller
@@ -22,5 +23,14 @@ public class Backend {
         model.addAttribute("mantenimiento", "productos");
         model.addAttribute("titulo", "Listado de categorías");
         return "backend/tables";
+    }
+
+    @GetMapping({"/productos"})
+    public String productos(Model model) {
+        ProductoService productoService = new ProductoService();
+        model.addAttribute("productos", productoService.findAll());
+        model.addAttribute("titulo","Tienda de ....");
+        model.addAttribute("contenido","productos");
+        return "backend/productos";
     }
 }
